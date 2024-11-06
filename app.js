5b09f61183b137d329dd8f59f0363a77cc71aff4
Alfandi Alamshach <fandixpc1@gmail.com>
2024-11-06 20:37:54 +0700

    selesai

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const todoRoutes = require('./routes/todo.js')
-const port = 3000;
+const todoRoutes = require('./routes/tododb.js')
+const port = process.env.PORT;
 
 app.use(express.json());
 
